0ad302632498249a89c33b60418ca928fb2dc4be
Sai Ram Kota <Sai.Ram.Kota@macys.com>
2016-10-20 12:51:57 -0700

    Added PropTypes/Firebase Integration
    Integrated with Mock User EndPoint in FireBase

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -2,11 +2,21 @@
  * Created by m853684 on 10/19/16.
  */
 var React=require('react');
+var NotesList=require("./NotesList");
 var Notes=React.createClass({
+    propTypes:{
+        username:React.PropTypes.string.isRequired,
+        notes:React.PropTypes.array.isRequired
+    },
     render:function(){
+       console.log(this.props.notes)
       return(
-          <div> Notes
-      <p>{this.props.notes}</p>
+          <div>
+              <h3> Notes for {this.props.username}</h3>
+
+              <div>
+                  <NotesList notes={this.props.notes}/>
+              </div>
           </div>
         )
     }
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,8 +3,11 @@ var Router=require('react-router');
 var Repos=require('../components/GitHub/Repos');
 var UserProfile=require('./GitHub/UserProfile');
 var Notes=require('./Notes/Notes');
+var ReactFireMixin=require('reactfire');
+var FireBase=require('firebase');
 
 var Profile=React.createClass({
+    mixins:[ReactFireMixin],
     getInitialState:function(){
         return {
             bio:{ name:'sai kota'},
@@ -12,22 +15,30 @@ var Profile=React.createClass({
             repos:[1,2,3]
         }
     },
+    componentDidMount:function(){
+        this.ref=new Firebase('https://egg-notes.firebaseio.com/');
+        this.childRef=this.ref.child(this.props.params.username);
+        this.bindAsArray(this.childRef,'notes');
+    },
+    componentWillUnmount:function(){
+        this.unbind('notes');
+    },
     render:function(){
 
-        console.log(this.props)
         return (
             <div className="row">
                 <div className="col-md-4">
                   <UserProfile username={this.props.params.username} bio={this.state.bio}/>
                 </div>
                 <div className="col-md-4">
-                <Repos repos={this.state.repos}/>
+                <Repos username={this.props.params.username} repos={this.state.repos}/>
                 </div>
                 <div className="col-md-4">
-               <Notes notes={this.state.notes}/>
+               <Notes username={this.props.params.username} notes={this.state.notes}/>
                 </div>
             </div>
         )
     }
 });
 module.exports=Profile;
+
